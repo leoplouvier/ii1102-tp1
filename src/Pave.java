b12339b042f103f0ae1bff3f279207eb1319334b
leoplouvier <plouvierleo@gmail.com>
2016-02-01 12:29:31 +0100

    TP1

--- src/Pave.java
+++ src/Pave.java
@@ -1,11 +1,27 @@
 import java.util.Scanner;
 
-
-public class Pave {
-    public static void main(String ... args) {
-
-	//1. Demander chaque longueur grâce au Scanner
+public class Pave {	
+	public static void main(String [] args) {
 	Scanner scan = new Scanner(System.in);
 	
-    }
+	double profondeur = scan.nextDouble();
+	if (profondeur<0) {
+		println("Valeur négative!!!! " );
+		double profondeur = scan.nextDouble();
 }
+	double largeur = scan.nextDouble();
+	if (largeur<0) {
+		println("Valeur négative!!!! " );
+		double largeur = scan.nextDouble();
+}
+	double longueur = scan.nextDouble();
+	if (longueur<0) {
+		println("Valeur négative!!!! " );
+		double longueur = scan.nextDouble();
+}
+	double volume = profondeur*largeur*longueur;
+	System.out.println("Le volume est " + volume);
+	
+	}
+}
+
